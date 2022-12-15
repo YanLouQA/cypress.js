1a548c4094cba435d4d9be4844dbcf17ecde7f84
Yanina <118587621+YanLouQA@users.noreply.github.com>
2022-12-15 05:38:13 +0300

    Update loginautotest.js

--- cypress/integration/Hawaii-task/loginautotest.js
+++ cypress/integration/Hawaii-task/loginautotest.js
@@ -10,6 +10,20 @@ describe('Проверка авторизации', function () {
 })
 
 
+describe('Проверка добавления товара', function () {
+   it('Позитивный кейс: После авторизации перейти в каталог и добавить товар в корзину', function () {
+      cy.contains('Каталог').click({force: true});
+      cy.get('[href="/catalog/collars"] > span').click();
+      cy.get(':nth-child(1) > .product-list > :nth-child(1) > .SProductCard__body > .SProductCard__name > .SProductCard__link').click();
+      cy.get('.SSpoiler__container > :nth-child(3)').click();
+      cy.get('[style="--Param-columns:1;"] > .SSpoiler > .SSpoiler__container > .SRadioButton > .option__name').click();
+      cy.get('.CTA').click();
+      cy.contains('Оформить заказ');
+      cy.contains('Продолжить покупки').click();
+      cy.contains('Мой профиль').click({force: true});
+    })
+})
+
 
 describe('Проверка ошибки авторизации', function () {
    it('Негативный кейс: верный логин и неверный пароль', function () {
@@ -21,4 +35,3 @@ describe('Проверка ошибки авторизации', function () {
       cy.get('.auth-form__submit > .s-button__content').click();
       cy.contains('Невозможно войти с предоставленными учетными данными.');
     })
-})
