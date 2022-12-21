80d67c63ee58fbcfebdc98531ab562ee084fe931
Yanina <118587621+YanLouQA@users.noreply.github.com>
2022-12-21 12:07:19 +0300

    Update qa_autotest.js

--- cypress/integration/Hawaii-task/qa_autotest.js
+++ cypress/integration/Hawaii-task/qa_autotest.js
@@ -1,8 +1,16 @@
+// Test Goals:
+// 1. Testing the password recovery form;
+// Authorization form testing:
+// 2. Positive case: correct login and password;
+// 3. Negative case: correct login and wrong password;
+// 4. Negative case: wrong login and correct password;
+
+
 describe('Восстановление пароля', function () {
    it('Проверка логики восстановления пароля', function () {
       cy.visit('https://login.qa.studio/');
       cy.get('#forgotEmailButton').click();
-      cy.get('#mailForgot').type('german@dolnikov.ru');
+      cy.get('#mailForgot').type('correct mail');
       cy.get('#restoreEmailButton').click();
       cy.contains('Успешно отправили пароль на e-mail');
       cy.get('#exitMessageButton > .exitIcon').click();
@@ -13,8 +21,8 @@ describe('Восстановление пароля', function () {
 describe('Проверка авторизации', function () {
    it('Позитивный кейс: валидные логин и пароль', function () {
       cy.reload();
-      cy.get('#mail').type('german@dolnikov.ru');
-      cy.get('#pass').type('iLoveqastudio1');
+      cy.get('#mail').type('correct mail');
+      cy.get('#pass').type('correct password');
       cy.get('#loginButton').click();
       cy.contains('Авторизация прошла успешно');
       cy.get('#exitMessageButton > .exitIcon').click();
@@ -25,8 +33,8 @@ describe('Проверка авторизации', function () {
 describe('Проверка авторизации', function () {
    it('Негативный кейс: верный логин и неверный пароль', function () {
       cy.reload();
-      cy.get('#mail').type('german@dolnikov.ru');
-      cy.get('#pass').type('iLoveqastudio2');
+      cy.get('#mail').type('correct mail');
+      cy.get('#pass').type('incorrect password');
       cy.get('#loginButton').click();
       cy.contains('Такого логина или пароля нет');
       cy.get('#exitMessageButton > .exitIcon').click();
@@ -37,8 +45,8 @@ describe('Проверка авторизации', function () {
 describe('Проверка авторизации', function () {
    it('Негативный кейс: неверный логин и верный пароль', function () {
       cy.reload();
-      cy.get('#mail').type('germ@dolnikov.ru');
-      cy.get('#pass').type('iLoveqastudio1');
+      cy.get('#mail').type('incorrect mail');
+      cy.get('#pass').type('correct password');
       cy.get('#loginButton').click();
       cy.contains('Такого логина или пароля нет');
       cy.get('#exitMessageButton > .exitIcon').click();
